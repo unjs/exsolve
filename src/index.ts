c66fc9c684e5300395fee6c1c1f874f88efe85f1
Mr. Robot <gomexxxx71@gmail.com>
2025-02-26 00:23:29 +0600

    chore: combine exports

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 export {
+  type ResolveOptions,
+  type ResolverOptions,
   resolveModuleURL,
   resolveModulePath,
   createResolver,
   clearResolveCache,
 } from "./resolve.ts";
 
-export type { ResolveOptions, ResolverOptions } from "./resolve.ts";
+
