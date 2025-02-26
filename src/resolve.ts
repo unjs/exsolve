eb5dcf2bebb6e4ab0c39d21d4509c7858d5f450f
Daniel Roe <daniel@roe.dev>
2025-02-26 07:56:41 +0000

    perf: use faster algo

--- src/resolve.ts
+++ src/resolve.ts
@@ -313,7 +313,7 @@ function _join(a: string, b: string): string {
   if (!a || !b || b === "/") {
     return a;
   }
-  return a.replace(/\/$/, "") + "/" + b.replace(/^\//, "");
+  return (a.endsWith("/") ? a : a + "/") + (b.startsWith("/") ? b.slice(1) : b);
 }
 
 function _parseInput(
